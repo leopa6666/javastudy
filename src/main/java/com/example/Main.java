58c8d69c35750a10c50bbb87275d515c169a40f3
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-26 13:52:09 +0900

    'javacommit2'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -78,15 +78,16 @@ public class Main {
     //HttpsURLConnection.setDefaultHostnameVerifier(hv);
     URL url = new URL("http://api.calil.jp/library");
     HttpURLConnection urlConn = (HttpURLConnection) url.openConnection();
-    System.out.println("sending request...");
-    urlConn.setRequestMethod("POST");
-    urlConn.setAllowUserInteraction(false); // no user interaction
-    urlConn.setDoOutput(true); // want to send
     Map headerFields = urlConn.getHeaderFields();
     OutputStreamWriter out = new OutputStreamWriter(urlConn.getOutputStream(),StandardCharsets.UTF_8);
     out.write("appkey=eff2329beb9938a9b6443b5795ff2db1&pref=埼玉県");
     out.flush();
     out.close();
+    System.out.println("sending request...");
+    urlConn.setRequestMethod("POST");
+    urlConn.setAllowUserInteraction(false); // no user interaction
+    urlConn.setDoOutput(true); // want to send
+
 
     urlConn.connect();
     System.out.println("header fields are: " + headerFields);
