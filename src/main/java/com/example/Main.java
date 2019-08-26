24a14384454ac081aee7dd095d56aa19ca92777b
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-26 10:53:09 +0900

    'javacommit2'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -66,6 +66,39 @@ public class Main {
 
   @RequestMapping("/calilrec")
   String calil() throws Exception {
+
+    System.setProperty("javax.net.ssl.trustStore", "jssecacerts.cert");
+
+    HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    URL url = new
+      URL("http://api.calil.jp/library"+
+          "?appkey=eff2329beb9938a9b6443b5795ff2db1&pref=埼玉県");
+    HttpsURLConnection urlConn = (HttpsURLConnection) url.openConnection();
+
+    System.out.println("sending request...");
+    urlConn.setRequestMethod("GET");
+    urlConn.setAllowUserInteraction(false); // no user interaction
+    urlConn.setDoOutput(true); // want to send
+    urlConn.setRequestProperty( "Content-type", "text/xml" );
+    urlConn.setRequestProperty( "accept", "text/xml" );
+    urlConn.setRequestProperty( "authorization", "Basic " + 
+        encode("administrator:collation"));
+    Map headerFields = urlConn.getHeaderFields();
+    System.out.println("header fields are: " + headerFields);
+
+    int rspCode = urlConn.getResponseCode();
+    if (rspCode == 200) {
+        InputStream ist = urlConn.getInputStream();
+        InputStreamReader isr = new InputStreamReader(ist);
+        BufferedReader br = new BufferedReader(isr);
+    
+        String nextLine = br.readLine();
+        while (nextLine != null) {
+            System.out.println(nextLine);
+            nextLine = br.readLine();
+        }
+    }
+    /*
     URL url = new URL("http://api.calil.jp/library");
     System.out.println(url);
     HttpURLConnection conn = (HttpURLConnection)url.openConnection();
@@ -88,7 +121,7 @@ public class Main {
     for(int i=0; i<nodes.getLength();i++)
     {
         System.out.println(nodes.item(i).getTextContent());
-    }
+    }*/
     return "index";
   }
 
