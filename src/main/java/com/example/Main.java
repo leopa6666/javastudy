9d5e563d5ed89e1b5fc4352ca271f38e80517160
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-26 16:43:24 +0900

    'javatest1'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -77,12 +77,11 @@ public class Main {
     System.setProperty("javax.net.ssl.trustStore", "jssecacerts.cert");
 
     //HttpsURLConnection.setDefaultHostnameVerifier(hv);
-    URL url = new URL("https://api.calil.jp/library?appkey=eff2329beb9938a9b6443b5795ff2db1&pref=埼玉県");
+    //URL url = new URL("https://api.calil.jp/library?appkey=eff2329beb9938a9b6443b5795ff2db1&pref=埼玉県");
+    URL url = new URL("https://api.calil.jp/library");
     HttpsURLConnection urlConn = (HttpsURLConnection) url.openConnection();
     System.out.println("sending request...");
     urlConn.setRequestMethod("GET");
-    urlConn.setAllowUserInteraction(false); // no user interaction
-    urlConn.setDoOutput(true);
     //OutputStreamWriter out = new OutputStreamWriter(urlConn.getOutputStream(),StandardCharsets.UTF_8);
     //out.write("?appkey=eff2329beb9938a9b6443b5795ff2db1&pref=埼玉県");
     //out.flush();
