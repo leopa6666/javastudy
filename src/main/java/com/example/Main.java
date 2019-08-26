2927832f12f4ae63daf10c135ae248b038251b71
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-26 11:31:23 +0900

    'javacommit2'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;//
-import java.net.HttpsURLConnection;
+import javax.net.ssl.HttpsURLConnection;
 import java.net.HttpURLConnection;//
 import java.net.URL;
 import java.io.BufferedReader;
