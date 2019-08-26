b8f56dc509de155612fb76d41cb2549a41b69fcc
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-26 11:26:16 +0900

    'javacommit2'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
