6263fcc090df28ae84068c6a63ed0f8a72759921
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-26 13:38:09 +0900

    'javacommit2'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -84,7 +84,7 @@ public class Main {
     urlConn.setAllowUserInteraction(false); // no user interaction
     urlConn.setDoOutput(true); // want to send
     Map headerFields = urlConn.getHeaderFields();
-    OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream(),StandardCharsets.UTF_8);
+    OutputStreamWriter out = new OutputStreamWriter(urlConn.getOutputStream(),StandardCharsets.UTF_8);
     out.write("?appkey=eff2329beb9938a9b6443b5795ff2db1&pref=埼玉県");
     out.flush();
     out.close();
