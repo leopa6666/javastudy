3f422fcdd2ad4c044a5fa90a754301e244529bc7
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-08-27 16:37:17 +0900

    'javatest'

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -97,7 +97,7 @@ public class Main {
         for(int i=0; i<nodes.getLength();i++)
         {
             System.out.println(nodes.item(i).getTextContent());
-            model1.put("nodes",nodes.item(i).getTextContent());
+            model.put("nodes",nodes.item(i).getTextContent());
         }
     }
     return "index";
