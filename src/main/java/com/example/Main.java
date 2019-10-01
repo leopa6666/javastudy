97b7c0936ffbb65d4a5ba267b5c116b3a4d89911
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-10-01 17:23:00 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -71,14 +71,14 @@ public class Main {
   
   public static void main(String[] args) throws Exception {
     testlist = MainConstants.address;
-    input_Sample = new Sample();
-    input_Sample.setInputAddress("");
     SpringApplication.run(Main.class, args);
   }
 
   @RequestMapping("/")
   String index(HttpServletRequest request, HttpServletResponse response) throws Exception {
     request.setAttribute("option", MainConstants.address);
+    input_Sample = new Sample();
+    input_Sample.setInputAddress("");
     //input_Sample.getInputAddress();
     return "index";
   }
