0b23d9a3743ad72b465003362146c8b846a6ab8b
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-10-04 10:56:57 +0900

    springtest1

--- src/main/java/com/example/LibSearch_ctrl.java
+++ src/main/java/com/example/LibSearch_ctrl.java
@@ -7,15 +7,6 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 @Scope("prototype")
-public class Sample {
-
-  private String input_address;
-
-  public String getInputAddress() {
-    return this.input_address;
-  }
-
-  public void setInputAddress(String address) {
-    this.input_address = address;
-  }
+public class LibSearch_ctrl {
+  
 }
