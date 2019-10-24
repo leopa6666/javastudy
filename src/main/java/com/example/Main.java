f3c99c4aac78c1274519710f6233d606ab031757
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-10-24 18:16:40 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -64,7 +64,7 @@ public class Main {
   @RequestMapping(path = "/content/{content_no}", method = RequestMethod.GET)
   String getarticle(HttpServletRequest request, HttpServletResponse response, @PathVariable String content_no) throws Exception {
     System.out.println(content_no);
-    if(content_no == "1"){
+    if(content_no.equals("1")){
       LibSerchResource input_SerchResource = new LibSerchResource();
       request.setAttribute("option", MainConstants.address);//都道府県
       request.setAttribute("input_info", input_SerchResource);
