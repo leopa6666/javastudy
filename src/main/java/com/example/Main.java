b20dc3791d80e9f20a03b939a6cb1fdafbfce4c3
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-11-19 18:26:12 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -74,7 +74,7 @@ public class Main {
       //request.setAttribute("keyword", "初期値");//都道府県
     }else if(content_no.equals("stresscheck")){
       StressCheckResource scResource = new StressCheckResource();
-      request.setAttribute("checkitems", scResource.checkitems);//都道府県
+      request.setAttribute("checkitems", scResource.checkitems);//質問マップ
     }
     return "article_" + content_no;
   }
--- src/main/java/com/example/StressCheckResource.java
+++ src/main/java/com/example/StressCheckResource.java
@@ -7,14 +7,24 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.TreeMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class StressCheckResource {
 
+  public List<String> checklist1 = new ArrayList<String>(){
+    add('当てはまる');
+    add('少し当てはまる');
+    add('どちらでもない');
+    add('あまり当てはまらない');
+    add('当てはまらない');
+  }
+
   public Map<String, String> checkitems = new TreeMap<String, String>() {
     {
-        put("質問1", "内容1");
-        put("質問2", "内容2");
+        put("質問1", checklist1);
+        put("質問2", checklist1);
     }
-};
+  };  
 }
