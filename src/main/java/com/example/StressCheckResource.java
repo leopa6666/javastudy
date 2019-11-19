5fdf5ee0270d944e4fc82d97cd2a9cdaa0ef3fee
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-11-19 17:42:40 +0900

    springtest1

--- src/main/java/com/example/StressCheckResource.java
+++ src/main/java/com/example/StressCheckResource.java
@@ -5,13 +5,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
 
 @Component
 public class StressCheckResource {
 
-  public Map<String, String> checkitems = new HashMap<String, String>() {
+  public Map<String, String> checkitems = new TreeMap<String, String>() {
     {
         put("質問1", "内容1");
         put("質問2", "内容2");
