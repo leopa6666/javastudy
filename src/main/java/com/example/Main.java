fd1a5142bc3a8400cf6045da070365a9b3656b7d
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-11-21 18:23:19 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -75,7 +75,7 @@ public class Main {
       //request.setAttribute("keyword", "初期値");//都道府県
     }else if(content_no.equals("stresscheck")){
       StressCheckResource scResource = new StressCheckResource();
-      model.addAttribute(new StressCheckResource());
+      request.setAttribute("resource",new StressCheckResource());
       request.setAttribute("checkitems", scResource.questionMap);//質問マップ
       request.setAttribute("answers", scResource.checklist1);//回答マップ
     }
