57f644058590f645a6295899439c6c30462da57d
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-11-27 16:15:23 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -83,7 +83,7 @@ public class Main {
   }
   @RequestMapping(value = "/contents/stresscheck", method = RequestMethod.POST)
   public ModelAndView setcheck(@ModelAttribute("resource") StressCheckForm form,ModelAndView mav,HttpServletRequest request) throws Exception {
-    mav = ctrl.get_stresscheck(form,mav,request);   
+    mav = ctrl.post_stresscheck(form,mav,request);   
     return mav;
   }
 
