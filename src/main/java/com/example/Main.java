938dcc866e89f0d830a81b4b95a19f11993a85e5
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-12-03 18:31:24 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -126,8 +126,8 @@ public class Main {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      //ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-      ResultSet rs = stmt.executeQuery("\\d");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      //ResultSet rs = stmt.executeQuery("\\d");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
