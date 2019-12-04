dca22f61dfa0283d2ce796162b9df6850032dece
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-12-04 17:14:17 +0900

    springtest1

--- src/main/java/com/example/Search_ctrlTests.java
+++ src/main/java/com/example/Search_ctrlTests.java
@@ -1,7 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 public class Search_ctrlTests {
     @Test
     public void test1() {
         LibSearch_ctrl ctrl = new LibSearch_ctrl();
-        
+
     }
 }
