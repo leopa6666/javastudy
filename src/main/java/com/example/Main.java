ad64d61e215f8527b6a12f7065550838c7b8c5f0
konnoyutaro <xkonno.yutaro@contract.isid.co.jp>
2019-12-05 13:53:25 +0900

    springtest1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -89,7 +89,7 @@ public class Main {
   //Swind get
   @RequestMapping(value = "/contents/swing", method = RequestMethod.GET)
   public String getswing(HttpServletRequest request, HttpServletResponse response) throws Exception {
-    Swing_ctrl ctrl = new Swing_ctrl();
+    //Swing_ctrl ctrl = new Swing_ctrl();
     //ctrl.get_swingreq();
     return "swing";
   }
