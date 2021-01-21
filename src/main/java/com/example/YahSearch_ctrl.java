74629e6019d71a0af851405857b20136af7bf683
leopa6666 <40862417+leopa6666@users.noreply.github.com>
2021-01-21 23:14:18 +0900

    yahoo検証

--- src/main/java/com/example/YahSearch_ctrl.java
+++ src/main/java/com/example/YahSearch_ctrl.java
@@ -62,7 +62,8 @@ public class YahSearch_ctrl {
       System.out.println("★"+root.get("hits"));
       for(Integer i=0; i < count ;i++){
         Map<String, String> infomap = new HashMap<>();
-        infomap.put("1" , root.get("hits").get("0").get("name").textValue();
+        System.out.println("★"+i);
+        infomap.put("1" , root.get("hits").get(i.toString()).get("name").textValue();
         //infomap.put("2" , root.get("hits").get("0").get("Result").get(i.toString()).get("Description").textValue());
         //infomap.put("3" , root.get("hits").get("0").get("Result").get(i.toString()).get("Url").textValue());
         //infomap.put("4" , root.get("hits").get("0").get("Result").get(i.toString()).get("Price").textValue());
